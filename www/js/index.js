f5f80db38285eaa8accb55f3d464d7838e8daef3
codegreenie <codegreenie@gmail.com>
2018-06-04 14:57:42 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,84 +1,25 @@
 function onDeviceReady() {
 
-    // Bind events
-    $(document).on("resume", onResume);
+   // this is the complete list of currently supported params you can pass to the plugin (all optional)
+var options = {
+  message: 'share this', // not supported on some apps (Facebook, Instagram)
+  subject: 'the subject', // fi. for email
+  files: ['', ''], // an array of filenames either locally or remotely
+  url: 'https://www.website.com/foo/#bar?a=b',
+  chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title,
+  appPackageName: 'com.apple.social.facebook' // Android only, you can provide id of the App you want to share with
+};
 
-    $('#location-settings').on("click", function(){
-        cordova.plugins.diagnostic.switchToLocationSettings();
-    });
+var onSuccess = function(result) {
+  alert("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+  alert("Shared to app: " + result.app); // On Android result.app since plugin version 5.4.0 this is no longer empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+};
 
+var onError = function(msg) {
+  alert("Sharing failed with message: " + msg);
+};
 
-    function onRequestSuccess(accuracyName, success){
-        handleSuccess("Successfully requested accuracy '"+accuracyName+"': "+success.message);
-    }
-
-    function onRequestFailure(error){
-        onError("Accuracy request failed: error code="+error.code+"; error message="+error.message);
-    }
-
-    $('#request-accuracy').on("click", function(){
-        var accuracy = $('#location-accuracy').val(),
-            accuracyName  =  $('#location-accuracy option:selected').text();
-        cordova.plugins.locationAccuracy.request(onRequestSuccess.bind(this, accuracyName), onRequestFailure, cordova.plugins.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY);
-    });
-
-    $('#request-authorization').on("click", function () {
-
-        cordova.plugins.diagnostic.isLocationAuthorized(function (authorized) {
-            if(!authorized){
-                cordova.plugins.diagnostic.requestLocationAuthorization(function (status) {
-                    handleSuccess("Requested location authorization: authorization was " + status);
-                }, onError, cordova.plugins.diagnostic.locationAuthorizationMode.ALWAYS);
-            }else{
-                onError("App is already authorized to use location");
-            }
-        }, onError);
-    });
-
-    checkState();
+window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
 }
 
-
-function checkState(){
-    console.log("Checking location state...");
-
-    cordova.plugins.locationAccuracy.canRequest(function(canRequest){
-        if(canRequest){
-            $('#request-accuracy').removeAttr('disabled');
-        }else{
-            $('#request-accuracy').attr('disabled', 'disabled');
-        }
-    });
-
-    cordova.plugins.diagnostic.isLocationAuthorized(function (authorized) {
-        if(authorized){
-            $('#request-authorization').attr('disabled', 'disabled');
-        }else{
-            $('#request-authorization').removeAttr('disabled');
-        }
-    }, onError);
-
-    function evaluateMode(mode){
-        $('#location-mode').text(mode.toUpperCase());
-    }
-    cordova.plugins.diagnostic.getLocationMode(evaluateMode, onError);
-}
-
-function onError(error) {
-    var msg = "An error occurred: " + error;
-    console.error(msg);
-    alert(msg);
-    checkState();
-}
-
-function handleSuccess(msg){
-    console.log(msg);
-    alert(msg);
-    checkState();
-}
-
-function onResume(){
-    checkState();
-}
-
-$(document).on("deviceready", onDeviceReady);
+document.addEventListener("deviceready", onDeviceReady, false);
